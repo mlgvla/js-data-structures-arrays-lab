a4201acb886ecf0eb00bbf09a2885808322e051e
mlgvla <mlgerardvla@gmail.com>
2021-07-06 20:52:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 // Write your solution here!
-// comment
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name); 
+}
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift();
+}
+
+function appendDriver(name) {
+    return [...drivers, name]
+}
+
+function prependDriver(name) {
+    return [name, ...drivers]
+}
+
+function removeLastDriver() {
+    return drivers.slice(0, drivers.length - 1)
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1)
+}
+
+
